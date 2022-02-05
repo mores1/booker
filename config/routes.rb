3eea3fe94ac27d398af685e2d1a59347151b02b7
EC2 Default User <ec2-user@ip-172-31-39-113.ec2.internal>
2022-02-05 15:28:43 +0000

    [途中]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  get 'books/top'
+  post 'books' => "books#create"
+  get 'books' => "books#index"
+  get 'books/:id' => "books#show",as: "book"
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => "books#update", as: "update_book"
+  get '/' => "homes#top"
   get 'homes/index'
   get 'homes/show'
   get 'homes/edit'
